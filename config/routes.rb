00f0cc3703a9b8dee391d3a26ecd192f7ee764dc
Louis Gesbert <louis.gesbert@hotmail.com>
2021-08-12 20:38:27 +0200

    création des routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :gardens, only: [:new, :index, :update, :show, :destroy] do
+    resources :square, only: [:new, :create, :update]
+    resources :plants, only: [:show]
+  end
 end
