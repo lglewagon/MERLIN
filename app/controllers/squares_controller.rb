35de303cc19035f775304c1a7c79be703c64baef
Matthieu de Villele <matthieu.devillele@gmail.com>
2021-08-14 11:15:06 +0200

    square controller done

--- app/controllers/squares_controller.rb
+++ app/controllers/squares_controller.rb
@@ -1,2 +1,24 @@
 class SquaresController < ApplicationController
+
+    def new 
+        @square = Square.new
+    end
+
+    def create 
+        @square = Square.new(params[:square])
+        @square.save
+    end
+
+    def update 
+        @square = Square.find(params[:id])
+        @square.update(params[:square])
+    end 
+
+    private
+
+  def square_params
+    params.require(:square).permit(:length_fix, :width_fix, :position_x, :position_y, :planting_date, :harvest_date, :garden_id, :plant_id)
+  end
+
+
 end
