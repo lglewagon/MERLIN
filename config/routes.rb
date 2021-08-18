150b9a66509a612ff6b9cce7225702837119e3ed
Louis Gesbert <louis.gesbert@hotmail.com>
2021-08-18 17:42:43 +0200

    ajout d'une root vers garden#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'gardens#new'
   resources :gardens, only: [:new, :index, :show, :destroy] do
     resources :squares, only: [:new, :create, :update, :show]
   end
