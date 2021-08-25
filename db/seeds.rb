9fdc4a5b80fb59909233d08af43316b6c6d967ec
Louis Gesbert <louis.gesbert@hotmail.com>
2021-08-25 11:43:28 +0200

    Seeds et image Done

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,8 +14,12 @@ Plant.create!(name: "Haricot", temperature_max: 32, temperature_min: 0, associat
 Plant.create!(name: "Patate", temperature_max: 32, temperature_min: 0, associations: {}, harvest_day_after_planting: 30, length: 5, width: 5, sunshine: 2, image: 'patate')
 Plant.create!(name: "Salade", temperature_max: 32, temperature_min: 0, associations: {}, harvest_day_after_planting: 30, length: 5, width: 5, sunshine: 2, image: 'salade')
 Plant.create!(name: "Tomate", temperature_max: 32, temperature_min: 0, associations: {}, harvest_day_after_planting: 30, length: 5, width: 5, sunshine: 2, image: 'tomate')
-Square.create!(length_fix: 10, width_fix: 10, position_x: 1, position_y: 1, plant_id: 0, garden_id: 1)
-Square.create!(length_fix: 10, width_fix: 10, position_x: 1, position_y: 1, plant_id: 1, garden_id: 1)
-Square.create!(length_fix: 10, width_fix: 10, position_x: 1, position_y: 1, plant_id: 2, garden_id: 1)
-Square.create!(length_fix: 10, width_fix: 10, position_x: 1, position_y: 1, plant_id: 3, garden_id: 1)
+Square.create!(length_fix: 10, width_fix: 10, position_x: 1, position_y: 1, planting_date: '25/08/2021', harvest_date: '25/12/2021', plant_id: 1, garden_id: 1)
+Square.create!(length_fix: 10, width_fix: 10, position_x: 1, position_y: 1, planting_date: '25/08/2021', harvest_date: '25/12/2021', plant_id: 1, garden_id: 1)
+Square.create!(length_fix: 10, width_fix: 10, position_x: 1, position_y: 1, planting_date: '25/08/2021', harvest_date: '25/12/2021', plant_id: 2, garden_id: 1)
+Square.create!(length_fix: 10, width_fix: 10, position_x: 1, position_y: 1, planting_date: '25/08/2021', harvest_date: '25/12/2021', plant_id: 3, garden_id: 1)
+Square.create!(length_fix: 10, width_fix: 10, position_x: 1, position_y: 1, planting_date: '25/08/2021', harvest_date: '25/12/2021', plant_id: 4, garden_id: 1)
+Square.create!(length_fix: 10, width_fix: 10, position_x: 1, position_y: 1, planting_date: '25/08/2021', harvest_date: '25/12/2021', plant_id: 4, garden_id: 1)
+Square.create!(length_fix: 10, width_fix: 10, position_x: 1, position_y: 1, planting_date: '25/08/2021', harvest_date: '25/12/2021', plant_id: 5, garden_id: 1)
+
 
