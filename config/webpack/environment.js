08428f2c5d46e840e1f30c4a0d08857f5b460fb8
Maria Bohorquez <a.maria.bohorquez@gmail.com>
2021-08-26 19:16:27 +0200

    le wagon setup

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,11 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default']
+  })
+)
 
 module.exports = environment
