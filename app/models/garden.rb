a13b20a40578557c0f8a2b195fa4ba211699a2f6
Matthieu de Villele <matthieu.devillele@gmail.com>
2021-09-11 13:50:00 +0200

    solved

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -10,7 +10,7 @@ class Garden < ApplicationRecord
         # Call API
         # weather_before_parsing = RequestWeatherServices.new(latitude, longitude).call
         # my_weather_hash = JSON.parse(weather_before_parsing)
-        
+
         # Parsing avec datas venant d'un fichier Json
         weather_test_before_parsing = File.read('app/weather_test.json')
         test_weather = JSON.parse(weather_test_before_parsing)
