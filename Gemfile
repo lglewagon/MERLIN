68e3a2f0fd61ee9437797c14f08268030759a963
Matthieu de Villele <matthieu.devillele@gmail.com>
2021-09-11 11:09:37 +0200

    wip

--- Gemfile
+++ Gemfile
@@ -3,8 +3,6 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.6.6'
 
-gem 'dotenv-rails', groups: [:development, :test]
-
 gem 'geocoder'
 
 gem 'autoprefixer-rails'
--- config/application.rb
+++ config/application.rb
@@ -15,10 +15,5 @@ module MERLIN
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    Bundler.require(*Rails.groups)
-
-    Dotenv::Railtie.load
-
-    HOSTNAME = ENV['HOSTNAME']
   end
 end
