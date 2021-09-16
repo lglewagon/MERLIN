b2995fd455c43e70111948031438a6c2ba4d1163
Matthieu de Villele <matthieu.devillele@gmail.com>
2021-09-16 18:56:42 +0200

    seed modificated & verif meteo all good plus de nil en latitude

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -8,11 +8,11 @@ class Garden < ApplicationRecord
 
     def weather
         # Call API
-        # weather_before_parsing = RequestWeatherServices.new(latitude, longitude).call
-        # my_weather_hash = JSON.parse(weather_before_parsing)
+        weather_before_parsing = RequestWeatherServices.new(latitude, longitude).call
+        my_weather_hash = JSON.parse(weather_before_parsing)
         
         # Parsing avec datas venant d'un fichier Json
-        weather_test_before_parsing = File.read('app/weather_test.json')
-        test_weather = JSON.parse(weather_test_before_parsing)
+        # weather_test_before_parsing = File.read('app/weather_test.json')
+        # test_weather = JSON.parse(weather_test_before_parsing)
     end
 end
