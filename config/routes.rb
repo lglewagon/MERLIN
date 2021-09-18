6b92a25059d37fd9b051e3f52c51719aaa313bfa
Matthieu de Villele <matthieu.devillele@gmail.com>
2021-09-18 11:31:50 +0200

    delete plant done

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'gardens#landing'
   resources :gardens, only: [:new, :create, :index, :show, :destroy] do
-    resources :plants, only: [:index]
+    resources :plants, only: [:index, :destroy]
   end
   resources :squares, only: [:show, :update]
 end
